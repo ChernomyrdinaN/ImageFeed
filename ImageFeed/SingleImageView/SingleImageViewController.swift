3c7803170db107e4835850a4e94fee6a40e04ae5
Natalya <chernomyrdina17@gmail.com>
2025-04-03 00:30:12 +0300

    Реализация модального способа презентации

--- ImageFeed/SingleImageView/SingleImageViewController.swift
+++ ImageFeed/SingleImageView/SingleImageViewController.swift
@@ -21,4 +21,7 @@ final class SingleImageViewController: UIViewController {
         super.viewDidLoad()
         imageView.image = image
     }
+    @IBAction private func didTapBackButton() {
+        dismiss(animated: true, completion: nil)
+    }
 }
