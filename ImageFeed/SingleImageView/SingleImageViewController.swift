07a2a55effac3fe0b778b8a34d0f129c39d7aad1
Natalya <chernomyrdina17@gmail.com>
2025-04-03 01:51:17 +0300

    Реализован Scroll View

--- ImageFeed/SingleImageView/SingleImageViewController.swift
+++ ImageFeed/SingleImageView/SingleImageViewController.swift
@@ -16,6 +16,7 @@ final class SingleImageViewController: UIViewController {
     }
     
     @IBOutlet private var imageView: UIImageView!
+    @IBOutlet private var scrollView: UIScrollView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
