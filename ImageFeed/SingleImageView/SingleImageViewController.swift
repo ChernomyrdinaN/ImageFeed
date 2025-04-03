ec47a348a3af52474536e5313049206adc0ddce3
Natalya <chernomyrdina17@gmail.com>
2025-04-04 00:15:53 +0300

    Реализован ZOOM

--- ImageFeed/SingleImageView/SingleImageViewController.swift
+++ ImageFeed/SingleImageView/SingleImageViewController.swift
@@ -10,8 +10,9 @@ import UIKit
 final class SingleImageViewController: UIViewController {
     var image: UIImage? {
         didSet {
-            guard isViewLoaded else { return }
+            guard isViewLoaded, let image else { return }
             imageView.image = image
+            imageView.frame.size = image.size
         }
     }
     
@@ -20,9 +21,18 @@ final class SingleImageViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        guard let image else { return }
         imageView.image = image
+        imageView.frame.size = image.size
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 1.25
     }
     @IBAction private func didTapBackButton() {
         dismiss(animated: true, completion: nil)
+    }
+}
+extension SingleImageViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
     }
 }
