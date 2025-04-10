340c5456929aa3b12617c77cc40a398782b9ae77
Natalya <chernomyrdina17@gmail.com>
2025-04-11 00:35:57 +0300

    Учтены рекомендации ревьюера: code style,guard,enum

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -6,7 +6,6 @@
 //
 import UIKit
 
-
 final class ProfileViewController: UIViewController {
     var profileImage: UIImageView?
     var nameLabel : UILabel?
@@ -16,7 +15,7 @@ final class ProfileViewController: UIViewController {
         static let white = UIColor(named: "YP White")
         static let gray = UIColor(named: "YP Gray")
         static let red = UIColor(named: "YP Red")
-      
+        
     }
     
     override func viewDidLoad() {
@@ -41,19 +40,18 @@ final class ProfileViewController: UIViewController {
         view.addSubview(profileImage)
         
         NSLayoutConstraint.activate([
-        profileImage.widthAnchor.constraint(equalToConstant: 70),
-        profileImage.heightAnchor.constraint(equalToConstant: 70),
-        profileImage.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor,constant: 16),
-        profileImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32)])
+            profileImage.widthAnchor.constraint(equalToConstant: 70),
+            profileImage.heightAnchor.constraint(equalToConstant: 70),
+            profileImage.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor,constant: 16),
+            profileImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32)])
         self.profileImage = profileImage
     }
     
-    
-   private func setUpNameLabel() {
+    private func setUpNameLabel() {
         
         let nameLabel = UILabel()
         guard let profileImage else { return }
-
+        
         nameLabel.text = "Наталья Черномырдина"
         nameLabel.font = UIFont.systemFont(ofSize: 23, weight: .bold)
         nameLabel.textColor = Colors.white
@@ -62,31 +60,31 @@ final class ProfileViewController: UIViewController {
         view.addSubview(nameLabel)
         
         NSLayoutConstraint.activate([
-        nameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 16),
-        nameLabel.topAnchor.constraint(equalTo: profileImage.bottomAnchor,constant: 8)])
+            nameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 16),
+            nameLabel.topAnchor.constraint(equalTo: profileImage.bottomAnchor,constant: 8)])
         self.nameLabel = nameLabel
     }
     
-   private func setUpLoginLabel() {
+    private func setUpLoginLabel() {
         let loginLabel = UILabel()
         guard let nameLabel else { return }
-       
+        
         loginLabel.text = "@chernomyrdina_nata"
         loginLabel.font = UIFont.systemFont(ofSize: 13, weight: .regular)
         loginLabel.textColor = Colors.gray
-
+        
         loginLabel.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(loginLabel)
         
         NSLayoutConstraint.activate([
-        loginLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-        loginLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8)])
+            loginLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            loginLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8)])
         self.loginLabel = loginLabel
     }
     
-  private func setUpDescriptionLabel() {
+    private func setUpDescriptionLabel() {
         let descriptionLabel = UILabel()
-        guard let loginLabel else {return}
+        guard let loginLabel else { return }
         
         descriptionLabel.text = "Hello, world!"
         descriptionLabel.font = UIFont.systemFont(ofSize: 13, weight: .regular)
@@ -96,11 +94,11 @@ final class ProfileViewController: UIViewController {
         view.addSubview(descriptionLabel)
         
         NSLayoutConstraint.activate([
-        descriptionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-        descriptionLabel.topAnchor.constraint(equalTo: loginLabel.bottomAnchor,constant: 8)])
+            descriptionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            descriptionLabel.topAnchor.constraint(equalTo: loginLabel.bottomAnchor,constant: 8)])
     }
     
-  private func setUpLogoutButton () {
+    private func setUpLogoutButton () {
         let logoutImage = UIImage(named: "logout")
         guard let logoutImage else { return }
         guard let profileImage else { return }
@@ -111,9 +109,9 @@ final class ProfileViewController: UIViewController {
         view.addSubview(logoutButton)
         
         NSLayoutConstraint.activate([
-        logoutButton.widthAnchor.constraint(equalToConstant: 44),
-        logoutButton.heightAnchor.constraint(equalToConstant: 44),
-        logoutButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor,constant: -16),
-        logoutButton.centerYAnchor.constraint(equalTo: profileImage.centerYAnchor)])
+            logoutButton.widthAnchor.constraint(equalToConstant: 44),
+            logoutButton.heightAnchor.constraint(equalToConstant: 44),
+            logoutButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor,constant: -16),
+            logoutButton.centerYAnchor.constraint(equalTo: profileImage.centerYAnchor)])
     }
 }
