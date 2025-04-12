985ed34ceb372ed7e24e7a969b8c26375d0fc91d
Natalya <chernomyrdina17@gmail.com>
2025-04-12 11:05:56 +0300

    Добавлен enum Constans:настроены доступы и ключи

--- ImageFeed/Resources/Constants.swift
+++ ImageFeed/Resources/Constants.swift
@@ -8,9 +8,9 @@
 import Foundation
 
 enum Constants {
-    static let accessKey = "<ваш Access Key>"
-    static let secretKey = "<ваш Secret Key>"
-    static let redirectURI = "<ваш Redirect URI>"
+    static let accessKey = "tc9RkTczPzHb5WGxs2r3PJQMNgxnY0Vm8U1kOscZZFY"
+    static let secretKey = "X1EFXKG0-u1SOAuE8TGQuqcXeXSUMRZF_ezUxUGZOWo"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
     static let accessScope = "public+read_user+write_likes" // список доступов
     static let defaultBaseURL = URL(string: "https://api.unsplash.com")! // базовый адрес API
 }
