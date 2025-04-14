4b27b5f3e789bf02981f7dd19182bf1de5a9751c
Natalya <chernomyrdina17@gmail.com>
2025-04-14 23:45:54 +0300

    Исправлены ошибки в кнопке Назад

--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -10,10 +10,6 @@ import WebKit
 final class WebViewViewController: UIViewController {
     
     @IBOutlet private var webView: WKWebView!
-
-    @IBAction private func didTapBackButton(_ sender: Any?) {
-
-    }
     
 }
     
