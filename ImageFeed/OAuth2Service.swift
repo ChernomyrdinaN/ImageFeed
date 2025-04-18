0fe44577b5ec74d6a6aa8042688be6cfbaff864d
Natalya <chernomyrdina17@gmail.com>
2025-04-18 16:38:48 +0300

    Добавлен NetworkClient

---
+++ ImageFeed/OAuth2Service.swift
@@ -0,0 +1,21 @@
+//
+//  Untitled.swift
+//  ImageFeed
+//
+//  Created by Наталья Черномырдина on 18.04.2025.
+// создать url
+
+/*import Foundation
+
+let client = NetworkClient()
+guard let url = URL(string: "https://api.example.com/data") else { return }
+
+client.fetch(url: url) { result in
+    switch result {
+    case .success(let data):
+        print("Данные получены: \(data)")
+    case .failure(let error):
+        print("Ошибка: \(error)")
+    }
+}
+*/
