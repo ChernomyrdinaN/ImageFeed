ce0c2df85ce911958cfb4352969f78484edc697e
Natalya <chernomyrdina17@gmail.com>
2025-05-08 13:39:27 +0300

    Полностью реализован View Controller

--- ImageFeed/TabBarController.swift
+++ ImageFeed/TabBarController.swift
@@ -5,7 +5,7 @@
 //  Created by Наталья Черномырдина on 08.05.2025.
 //
 import UIKit
- 
+
 final class TabBarController: UITabBarController {
     
     // MARK: - Lifecycle
@@ -13,18 +13,25 @@ final class TabBarController: UITabBarController {
         super.awakeFromNib()
         
         // MARK: - Setup ViewControllers
-            let storyboard = UIStoryboard(name: "Main", bundle: nil)
-           
-            // Создаем ImagesListViewController
-            let imagesListViewController = storyboard.instantiateViewController(withIdentifier: "ImagesListViewController") as! ImagesListViewController
-            // Создаем ProfileViewController
-            let profileViewController = storyboard.instantiateViewController(withIdentifier: "ProfileViewController") as! ProfileViewController
-            // Назначаем контроллеры
-            self .viewControllers = [imagesListViewController, profileViewController]
-            print("[TabBarController]: Успех - TabBarController настроен с 2 табами")
-            
-        }
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        //  Создаем ImagesListViewController
+        let imagesListViewController = storyboard.instantiateViewController(withIdentifier: "ImagesListViewController") as! ImagesListViewController
+        //  Создаем ProfileViewController
+        let profileViewController = ProfileViewController()
+        
+        //  Настраиваем tabBarItem
+        profileViewController.tabBarItem = UITabBarItem(
+        title: "",
+        image: UIImage(systemName: "person.circle"),
+        selectedImage: nil
+        )
+        
+        // Назначаем контроллеры
+        self .viewControllers = [imagesListViewController, profileViewController]
+        print("[TabBarController]: Успех - TabBarController настроен с 2 табами")
     }
+}
 
 
 
