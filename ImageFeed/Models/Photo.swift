5fd38233ae881d0835ff42f998e8a8cd8666b9b1
Natalya <chernomyrdina17@gmail.com>
2025-05-16 13:33:03 +0300

    Добавлены структуры для сетевого запроса  Photo и PhotoResult

---
+++ ImageFeed/Models/Photo.swift
@@ -0,0 +1,38 @@
+//
+//  Photo.swift
+//  ImageFeed
+//
+//  Created by Наталья Черномырдина on 16.05.2025.
+//  Модель отдельного экземпляра фотографии используется в UI приложения
+
+import UIKit
+
+// MARK: - UI Model
+struct Photo {
+    
+    // MARK: - Public Properties
+    let id: String            // Уникальный идентификатор
+    let size: CGSize          // Размеры изображения
+    let createdAt: Date?      // Дата создания (опционально)
+    let description: String?  // Описание фотографии (опционально)
+    let thumbImageURL: String // URL миниатюры
+    let largeImageURL: String // URL полноразмерного изображения
+    let isLiked: Bool         // Отмечено ли как понравившееся
+}
+
+// MARK: - API to UI Conversion
+extension Photo {
+    // Инициализатор для преобразования API-модели (PhotoResult) в UI-модель (Photo)
+    init(from result: PhotoResult) {
+        self.id = result.id
+        self.size = CGSize(width: result.width, height: result.height)
+        
+        let formatter = ISO8601DateFormatter()
+        self.createdAt = result.createdAt.flatMap { formatter.date(from: $0) }
+        
+        self.description = result.description
+        self.thumbImageURL = result.urls.thumb
+        self.largeImageURL = result.urls.full
+        self.isLiked = result.likedByUser
+    }
+}
