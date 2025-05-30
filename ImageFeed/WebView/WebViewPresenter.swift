8c66f18b60199e06a54fb22eaa429616f5318380
Natalya <chernomyrdina17@gmail.com>
2025-05-30 16:45:01 +0300

    Подготовка к рефакторингу WebViewViewController

---
+++ ImageFeed/WebView/WebViewPresenter.swift
@@ -0,0 +1,10 @@
+//
+//  WebViewPresenter.swift
+//  ImageFeed
+//
+//  Created by Наталья Черномырдина on 30.05.2025.
+//
+
+final class WebViewPresenter: WebViewPresenterProtocol {
+    weak var view: WebViewViewControllerProtocol?
+}
